fdcf6bd080ee0d8ba4f9b35e7ab36dea0e9ea807
IrynaKutsakova <i.kutsakova@gmail.com>
2023-09-07 04:25:10 +0300

    UC#1 Create endpoint

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using RestCountriesApp.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace RestCountriesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly string _countriesApiEndpoint;
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public CountriesController(IConfiguration configuration)
+        {
+            _countriesApiEndpoint = configuration.GetValue<string>("ApiUrls:RESTCountriesAPI");
+        }
+
+
+        // GET: api/countries?param1=value1&number=1&param3=value3
+        // If you call /api/countries? param1 = Canada, it will return countries with names that contain "Canada".
+        // If you call /api/countries? number = 50000000, it will return countries with a population of 50 million or more.
+        // If you call /api/countries? param3 = Asia, it will return countries in the Asia region.
+        [HttpGet]
+        public async Task<IActionResult> FetchCountries(string? param1 = null, int? number = null,
+            string? param3 = null)
+        {
+            var response = await HttpClient.GetStringAsync(_countriesApiEndpoint);
+            var countries = JsonConvert.DeserializeObject<List<CountryInfo>>(response);
+
+            if (!string.IsNullOrWhiteSpace(param1)) // This is for nameFilter
+            {
+                countries = countries.Where(c =>
+                    c.Name.Common.Contains(param1, StringComparison.OrdinalIgnoreCase) ||
+                    c.Name.Official.Contains(param1, StringComparison.OrdinalIgnoreCase)
+                ).ToList();
+            }
+
+            if (number.HasValue) // This is for populationFilter
+            {
+                countries = countries.Where(c => c.Population >= number.Value).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(param3)) // This is for regionFilter
+            {
+                countries = countries.Where(c =>
+                    c.Region.Equals(param3, StringComparison.OrdinalIgnoreCase)
+                ).ToList();
+            }
+
+            return Ok(countries);
+        }
+    }
+}
---
+++ Models/CountryInfo.cs
@@ -0,0 +1,58 @@
+﻿namespace RestCountriesApp.Models
+{
+    public class CountryInfo
+    {
+        public NameInfo Name { get; set; }
+
+        public List<string> Capital { get; set; }
+
+        // public List<string> Borders { get; set; }
+        public string Cioc { get; set; }
+        public Dictionary<string, Currency> Currencies { get; set; }
+        public string Demonym { get; set; }
+        public Flags Flags { get; set; }
+        public string Id { get; set; } // Note: In the API, this is the country's alpha-3 code
+        public bool Independent { get; set; }
+        public string Landlocked { get; set; }
+        public Dictionary<string, string> Languages { get; set; }
+        public Dictionary<string, string> Maps { get; set; }
+
+        public int Population { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public List<string> Tld { get; set; }
+        public Dictionary<string, TranslationDetail> Translations { get; set; }
+        public bool UnMember { get; set; }
+    }
+
+    public class NameInfo
+    {
+        public string Common { get; set; }
+        public string Official { get; set; }
+    }
+
+    public class Flags
+    {
+        public string Png { get; set; }
+        public string Svg { get; set; }
+    }
+
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Translation
+    {
+        public string Official { get; set; }
+        public string Common { get; set; }
+    }
+
+    public class TranslationDetail
+    {
+        public string SomeKey { get; set; }
+        // Add other properties based on the JSON structure
+    }
+}
